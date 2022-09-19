f1116463d1a5cc907fef09de244b7f21fa2ffbae
LucianoAmato <luciano2697@outlook.com.ar>
2022-09-19 20:23:57 -0300

    PreEntrega1Amato

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,22 +13,24 @@ const NavBar = () => {
   return (
     <div className="navbar text-warning text-3xl italic" style={navBarStyle}>
       <div className="flex-1">
-        <a>
+        <button>
           <img src={logo} alt='logo Dream-Burger' className='w-40 p-1'></img>
-        </a>
+        </button>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal p-0">
-          <li className='mx-3'><a className='active:bg-warning active:text-black hover:scale-125'>INICIO</a></li>
-          <li className='mx-3'><a className='active:bg-warning active:text-black hover:scale-125'>MENÚ</a></li>
-          <li className='mx-3'><a className='active:bg-warning active:text-black hover:scale-125'>CONTACTO</a></li>
-          <li className='mx-3'><a className='active:bg-warning active:text-black hover:scale-125'><CartWidget/></a></li>
+          <li className='mx-3'><button className='active:bg-warning active:text-black hover:scale-125'>INICIO</button></li>
+          <li className='mx-3'><button className='active:bg-warning active:text-black hover:scale-125'>MENÚ</button></li>
+          <li className='mx-3'><button className='active:bg-warning active:text-black hover:scale-125'>CONTACTO</button></li>
+          <li className='mx-3'><button className='active:bg-warning active:text-black hover:scale-125'><CartWidget/></button></li>
         </ul>
       </div>
     </div>
+
   );
 }
 
 export default NavBar;
 
 // SE HA UTILIZADO TAILWIND Y DAISYUI.
+// LAS ETIQUETAS <BUTTON> SE PODRAN CAMBIAR A ETIQUETAS <A> DEPENDIENDO DE LA FUNCIONALIDAD DESEADA.
